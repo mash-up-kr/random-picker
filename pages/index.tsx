b2fa28007465f983458240ef8f18050471c3c31a
Baek2back <dumbuk12@naver.com>
2022-08-21 01:58:18 +0900

    Feat: 3D 캐러셀 기본 골격을 구현한다

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,89 @@
 import type { NextPage } from 'next';
-import { Alert, AlertIcon, AlertTitle, AlertDescription } from '@chakra-ui/react';
-import Test from '@/components/common/Test';
+import { Container, Box, Button, VStack, ButtonGroup, Flex } from '@chakra-ui/react';
+import { css } from '@emotion/react';
+import { useState } from 'react';
 
 const Home: NextPage = () => {
+  const [items, setItems] = useState(() => [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const [currentIdx, setCurrentIdx] = useState(0);
+  const [intervalId, setIntervalId] = useState<ReturnType<typeof setInterval>>();
   return (
-    <>
-      <Alert status="error">
-        <AlertIcon />
-        <AlertTitle>Your browser is outdated!</AlertTitle>
-        <AlertDescription>Your Chakra experience may be degraded.</AlertDescription>
-      </Alert>
-      <Test></Test>
-    </>
+    <Flex justifyContent="center" alignItems="center" style={{ width: '100vw', height: '100vh' }}>
+      <VStack spacing="48px">
+        <Container
+          css={css`
+            background: red;
+            margin: 0 auto;
+            width: 250px;
+            height: 200px;
+            position: relative;
+            perspective: 2000px;
+          `}
+        >
+          <Box
+            css={css`
+              width: 100%;
+              height: 100%;
+              position: absolute;
+              transform-style: preserve-3d;
+            `}
+          >
+            {items.map((item, idx) => {
+              return (
+                <Box
+                  key={idx}
+                  css={css`
+                    position: absolute;
+                    background: #000;
+                    width: 150px;
+                    height: 200px;
+
+                    font-size: 5em;
+                    text-align: center;
+                    color: #fff;
+                    opacity: 0.95;
+                    border-radius: 10px;
+                    transition: transform 2s;
+                    perspective: 500px;
+                    transform: rotateY(
+                        ${idx * (360 / items.length) + currentIdx * (360 / items.length)}deg
+                      )
+                      translateZ(250px);
+                  `}
+                >
+                  {item}
+                </Box>
+              );
+            })}
+          </Box>
+        </Container>
+        <ButtonGroup>
+          <Button
+            onClick={() => {
+              if (!intervalId) {
+                setIntervalId(
+                  setInterval(() => {
+                    setCurrentIdx((idx) => idx + 1);
+                  }, 100),
+                );
+              }
+            }}
+          >
+            시작
+          </Button>
+          <Button
+            onClick={() => {
+              if (intervalId) {
+                setIntervalId(clearInterval(intervalId) as undefined);
+              }
+            }}
+          >
+            정지
+          </Button>
+        </ButtonGroup>
+      </VStack>
+      <div>{}</div>
+    </Flex>
   );
 };
 
