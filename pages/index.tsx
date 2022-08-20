63b9710efc81b80f100aa90fcd1ed4df37e60d42
HaJunRyu <hjr4557@naver.com>
2022-08-21 02:06:56 +0900

    Feat: 캐러셀 아이템을 서비스 이미지로 변경한다

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,23 @@ import type { NextPage } from 'next';
 import { Container, Box, Button, VStack, ButtonGroup, Flex } from '@chakra-ui/react';
 import { css } from '@emotion/react';
 import { useState } from 'react';
+import affa from '@/assets/images/에프파.png';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
-  const [items, setItems] = useState(() => [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const [items, setItems] = useState(() => [affa, affa, affa, affa, affa, affa, affa, affa]);
   const [currentIdx, setCurrentIdx] = useState(0);
   const [intervalId, setIntervalId] = useState<ReturnType<typeof setInterval>>();
+
   return (
-    <Flex justifyContent="center" alignItems="center" style={{ width: '100vw', height: '100vh' }}>
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      style={{ width: '100vw', height: '100vh', background: '#000' }}
+    >
       <VStack spacing="48px">
         <Container
           css={css`
-            background: red;
             margin: 0 auto;
             width: 250px;
             height: 200px;
@@ -34,13 +40,13 @@ const Home: NextPage = () => {
                   key={idx}
                   css={css`
                     position: absolute;
-                    background: #000;
+                    background: #fff;
                     width: 150px;
                     height: 200px;
 
                     font-size: 5em;
                     text-align: center;
-                    color: #fff;
+                    color: #000;
                     opacity: 0.95;
                     border-radius: 10px;
                     transition: transform 2s;
@@ -51,7 +57,7 @@ const Home: NextPage = () => {
                       translateZ(250px);
                   `}
                 >
-                  {item}
+                  <Image src={item.src} layout="fill"></Image>
                 </Box>
               );
             })}
