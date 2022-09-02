257a389daab35fcaf19b68b7d3bdc76fb4cb2f21
HaJunRyu <hjr4557@naver.com>
2022-09-02 11:25:36 +0900

    Style: home page Container의 padding을 제거해준다

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { RandomPicker } from '@/components';
 function Home() {
   return (
     <>
-      <Container display="flex" maxW="100%">
+      <Container display="flex" maxW="100%" p={0}>
         <RandomPicker />
       </Container>
     </>
